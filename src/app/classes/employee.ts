86d191729e4a4e6e8cd5ae1239c5fa9a6dc91df1
FatmaElhosary <engfatmafathy@gmail.com>
2020-10-28 15:13:38 +0200

    first commit

---
+++ src/app/classes/employee.ts
@@ -0,0 +1,15 @@
+class Employee{
+    id:string;
+    fullName_FL:string;
+    fullName_SL:string;
+    hiringDate:Date;
+    firstContractingSalary:number;
+ 
+}
+class  Position{
+
+}
+module.exports={
+    Employee,
+    Position
+}
